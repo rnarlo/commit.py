f42ea14485ade78f3e822248166a68e4dfda5df1
chr1s <jimenezmarlo1112@gmail.com>
2021-09-13 18:02:13 +0800

    update script

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
 import os
 
 commitNos = int(input("Commit for this number of times: "))
+counter = 0
 
 for i in range(commitNos):
     os.system(f'git commit --allow-empty -m "Commit number: {i+1}"')
-    
-os.system('git push')
+    counter += 1
+    if(counter == 5000):
+        os.system('git push')
+        counter -= 5000
 
-#inspired by Virej Dasani! Check them out :)
+
+# inspired by Virej Dasani! Check them out :)
